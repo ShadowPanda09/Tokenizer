f9c778605186ded88c62d88254fbdb589bb7cb09
Ryan Schutte <rmschutte09@gmail.com>
2026-02-23 12:15:14 -0500

    heeeeelp

--- tokenizers-main/tokenizer.py
+++ tokenizers-main/tokenizer.py
@@ -3,34 +3,20 @@ from tokenizers.pre_tokenizers import Whitespace
 from tokenizers.normalizers import Lowercase, NFD, StripAccents, Sequence
 from tokenizers.trainers import BpeTrainer
 import pandas as pd
-import torch as tr
+import torch
 from transformers import pipeline, AutoTokenizer
 
-tokenizer = AutoTokenizer.from_pretrained("google/gemma-2-2b") 
-text = input("Prompt here")
-tokenizer(text, return_tensors = "pt")
+comp = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(comp)
 
 
+pipe = pipeline(
+    "text-generation",
+    model = "FacebookAI/roberta-large-mnli",
+    device = comp
+)
 
-
-
-"""
-df = pd.read_csv("cleanedjeopardy.csv")
-text_data = df['Answer'].tolist()
-
-tokenizer = Tokenizer(models.BPE(unk_token="[UNK]"))
-
-tokenizer.normalizer = Sequence([NFD(), Lowercase(), StripAccents()])
-tokenizer.pre_tokenizers = Whitespace()
-
-trainer = BpeTrainer(special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"])
-
-
-
-tokenizer.train(text_data,trainer)
-
-
-
-output= tokenizer.encode("Hello, y'all! How are you 😁 ?")
-print(output.tokens)
-"""
+text = input("")
+outputs = pipe(text, max_new_tokens = 256)
+response = outputs[0]["generated_text"]
+print(response)
